9941d5e1eb481ee5e6388d25c17e48df167529b6
fengys <1113014250@qq.com>
2018-10-02 21:27:12 +0800

    #002 logininterceptor的编写

---
+++ src/main/java/com/dlut/interceptor/LoginInterceptor.java
@@ -0,0 +1,37 @@
+package com.dlut.interceptor;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator on 2018/10/2 0002.
+ */
+public class LoginInterceptor implements HandlerInterceptor
+{
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception
+    {
+        logger.debug("preHandle method is running");
+        //System.out.println("");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception
+    {
+        logger.debug("postHandle method is running");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception
+    {
+        logger.debug("afterHandle method is running");
+    }
+}
