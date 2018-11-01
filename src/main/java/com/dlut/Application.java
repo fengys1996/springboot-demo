5d0cdae2212483eb4b2f0026e063fbf49bf36248
fys <1113014250@qq.com>
2018-11-01 22:28:21 +0800

    122

--- src/main/java/com/dlut/Application.java
+++ src/main/java/com/dlut/Application.java
@@ -2,8 +2,11 @@ package com.dlut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableConfigurationProperties
 public class Application
 {
     public static void main(String[] args)
--- src/main/java/com/dlut/utils/Emailutils.java
+++ src/main/java/com/dlut/utils/Emailutils.java
@@ -4,6 +4,10 @@ import com.sun.mail.util.MailSSLSocketFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -16,30 +20,49 @@ import java.security.GeneralSecurityException;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+
 public class Emailutils {
     private static final Logger logger = LoggerFactory.getLogger(Emailutils.class);
 
     //@Autowired
     private Environment env;
 
-//    private static String auth;
-//    private static String host;
-//    private static String protocol;
-//    private static int port;
-//    private static String authName;
-//    private static String password;
-//    private static boolean isSSL;
-//    private static String charset ;
-//    private static String timeout;
-    private static String auth = "true";
-    private static String host = "smtp.163.com";
-    private static String protocol = "smtp";
-    private static int port = 465;
-    private static String authName = "15536886356@163.com";
-    private static String password = "522975519";
-    private static boolean isSSL = true;
-    private static String charset = "UTF-8";
-    private static String timeout = "5000";
+    @Value("${mail.smtp.auth}")
+    private static String auth;
+
+    @Value("${mail.host}")
+    private static String host;
+
+    @Value("${mail.transport.protocol}")
+    private static String protocol;
+
+    @Value("${mail.smtp.port}")
+    private static int port;
+
+    @Value("${mail.auth.name}")
+    private static String authName;
+
+    @Value("${mail.auth.password}")
+    private static String password;
+
+    @Value("${mail.is.ssl}")
+    private static boolean isSSL;
+
+    @Value("${mail.send.charset}")
+    private static String charset ;
+
+    @Value("${mail.smtp.timeout}")
+    private static String timeout;
+
+    //private static String auth = "true";
+    //private static String host = "smtp.163.com";
+    //private static String protocol = "smtp";
+    //private static int port = 465;
+    //private static String authName = "15536886356@163.com";
+    //private static String password = "522975519";
+    //private static boolean isSSL = true;
+    //private static String charset = "UTF-8";
+    //private static String timeout = "5000";
 
     /*@PostConstruct
     public void initParam () {
@@ -56,6 +79,7 @@ public class Emailutils {
     }*/
 
     public static boolean sendEmail(String subject, String[] toUsers, String[] ccUsers, String content, List<Map<String, String>> attachfiles) {
+        System.out.println(charset + "," + timeout );
         boolean flag = true;
         try {
             JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
